c0728956e42763616c9acb3f56e1b4b04719bb43
er.rajatmalik <er.rajatmalik@gmail.com>
2020-07-02 22:12:25 +0530

    inti cake php 4

---
+++ src/Controller/AmenityController.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Http\Response;
+
+class AmenityController extends AppController
+{
+   
+    public function index()
+    {
+       pr('hello');
+    }
+}
